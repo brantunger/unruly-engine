a9fc45794a309cdcb5ba0386d7128b4aa88b8815
Brant Unger <brant.unger@cvshealth.com>
2023-04-03 04:51:07 -0700

    Remove namespace from repository

--- src/main/java/com/unruly/repository/RulesRepository.java
+++ src/main/java/com/unruly/repository/RulesRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @Repository
 public interface RulesRepository extends JpaRepository<RuleDbModel, Long> {
-    List<RuleDbModel> findByRuleNamespace(String ruleNamespace);
 
     List<RuleDbModel> findAll();
 }
